ed4e66a5f01d635c48f9396df37095aaa9bb1f6a
Emerson <emerson.matosokl10@gmail.com>
2024-08-01 22:37:50 -0300

    feat: alteração  server vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
 import { fileURLToPath } from 'node:url'
-import fs from 'node:fs'
 import path from 'node:path'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import fs from 'node:fs'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { defineConfig } from 'vite'
+import vuetify from 'vite-plugin-vuetify'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
-import vuetify from 'vite-plugin-vuetify'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  console.log('mode:', mode)
-
   const isDev = mode === 'development'
 
   const serverConfig = isDev
@@ -27,38 +24,23 @@ export default defineConfig(({ mode }) => {
       host: '192.168.18.27',
       port: 5173,
     }
-    : { port: 5173 }
+    : {
+      host: '0.0.0.0',
+      port: process.env.PORT || 5173,
+    }
 
   return {
     plugins: [
       vue(),
       vueJsx(),
       vuetify({
-        styles: {
-          configFile: 'src/styles/variables/_vuetify.scss',
-        },
+        styles: { configFile: 'src/styles/variables/_vuetify.scss' },
       }),
-      Pages({
-        dirs: ['./src/pages'],
-      }),
-      Layouts({
-        layoutsDirs: './src/layouts/',
-      }),
-      Components({
-        dirs: ['src/@core/components', 'src/views/demos', 'src/components'],
-        dts: true,
-      }),
-      AutoImport({
-        imports: ['vue', 'vue-router', '@vueuse/core', '@vueuse/math', 'vue-i18n', 'pinia'],
-        vueTemplate: true,
-      }),
-      VueI18nPlugin({
-        runtimeOnly: true,
-        compositionOnly: true,
-        include: [
-          fileURLToPath(new URL('./src/plugins/i18n/locales/**', import.meta.url)),
-        ],
-      }),
+      Pages({ dirs: ['./src/pages'] }),
+      Layouts({ layoutsDirs: './src/layouts/' }),
+      Components({ dirs: ['src/@core/components', 'src/views/demos', 'src/components'], dts: true }),
+      AutoImport({ imports: ['vue', 'vue-router', '@vueuse/core', '@vueuse/math', 'vue-i18n', 'pinia'], vueTemplate: true }),
+      VueI18nPlugin({ runtimeOnly: true, compositionOnly: true, include: [fileURLToPath(new URL('./src/plugins/i18n/locales/**', import.meta.url))] }),
       DefineOptions(),
     ],
     server: serverConfig,
@@ -77,14 +59,7 @@ export default defineConfig(({ mode }) => {
         'apexcharts': fileURLToPath(new URL('node_modules/apexcharts-clevision', import.meta.url)),
       },
     },
-    build: {
-      chunkSizeWarningLimit: 5000,
-    },
-    optimizeDeps: {
-      exclude: ['vuetify'],
-      entries: [
-        './src/**/*.vue',
-      ],
-    },
+    build: { chunkSizeWarningLimit: 5000 },
+    optimizeDeps: { exclude: ['vuetify'], entries: ['./src/**/*.vue'] },
   }
 })
