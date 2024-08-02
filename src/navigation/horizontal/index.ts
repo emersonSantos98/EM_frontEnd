2274bdd612932b7f5fa9b7e33b53a85b63dd5290
Emerson <emerson.matosokl10@gmail.com>
2024-08-02 12:16:49 -0300

    feat: Limpeza de template

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -1,15 +1,12 @@
 import home from './home'
-import secondPage from './secondPage'
 import calculator from './calculator'
 import rolePermissions from './role&Permissions'
-import profile from './profile'
+
 
 import type { VerticalNavItems } from '@/@layouts/types'
 
 export default [
   ...home,
-  ...secondPage,
   ...calculator,
   ...rolePermissions,
-  ...profile,
 ] as VerticalNavItems
