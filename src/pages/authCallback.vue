b8f371dffe903d90719a8f6088b31405dbe8f0e2
Emerson <emerson.matosokl10@gmail.com>
2024-08-16 18:26:01 -0300

    chore: alteração de feth para axios

--- src/pages/authCallback.vue
+++ src/pages/authCallback.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import axios from 'axios'
 import { useAuthStore } from '@/store/auth'
 import { useAppAbility } from '@/plugins/casl/useAppAbility'
 import Notifier from '@core/utils/Notifier'
@@ -12,15 +13,10 @@ const router = useRouter()
 
 onMounted(async () => {
   try {
-    // Faz a requisição para verificar o sucesso do login
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login/success`, {
-      credentials: 'include',
-    })
-
-    const result = await response.json()
-
-    if (result && !result.error) {
-      const { user } = result
+    const url = `${import.meta.env.VITE_API_URL}/auth/login/success`
+    const { data } = await axios.get(url, { withCredentials: true })
+    if (data && !data.error) {
+      const { user } = data
 
       ability.update(user.userAbilities as any)
       authUser.setUser(user.userData)
@@ -30,7 +26,6 @@ onMounted(async () => {
       authUser.setAbilities(user.userAbilities)
       authUser.setIsAuth(true)
       notifier.success('Login realizado com sucesso!')
-
       router.push({ name: 'index' })
     }
     else {
@@ -38,9 +33,38 @@ onMounted(async () => {
     }
   }
   catch (err) {
-    console.error('Erro no callback do Google:', err)
-    router.push({ name: 'login' }) // Redireciona para o login em caso de erro
+    console.log(err)
   }
+  // try {
+  //   // Faz a requisição para verificar o sucesso do login
+  //   const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login/success`, {
+  //     credentials: 'include',
+  //   })
+  //
+  //   const result = await response.json()
+  //
+  //   if (result && !result.error) {
+  //     const { user } = result
+  //
+  //     ability.update(user.userAbilities as any)
+  //     authUser.setUser(user.userData)
+  //     authUser.setDadosTemporario(user.refreshToken, user.accessToken, user.userAbilities)
+  //     authUser.setToken(user.accessToken)
+  //     authUser.setRefreshToken(user.refreshToken)
+  //     authUser.setAbilities(user.userAbilities)
+  //     authUser.setIsAuth(true)
+  //     notifier.success('Login realizado com sucesso!')
+  //
+  //     router.push({ name: 'index' })
+  //   }
+  //   else {
+  //     throw new Error('Erro ao processar o login com Google')
+  //   }
+  // }
+  // catch (err) {
+  //   console.error('Erro no callback do Google:', err)
+  //   router.push({ name: 'login' }) // Redireciona para o login em caso de erro
+  // }
 })
 </script>
 
