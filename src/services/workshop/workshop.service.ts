869fcf646a94f3bd3151f7260a7609d6ede518c5
Emerson <emerson.matosokl10@gmail.com>
2024-11-27 22:54:21 -0300

    git add src/services/workshop/workshop.service.ts git commit -m "feat(service): integra endpoints de parceiros com axios
    - Implementa métodos para adicionar, buscar, atualizar e deletar parceiros.
    - Integra com os endpoints da API."

---
+++ src/services/workshop/workshop.service.ts
@@ -0,0 +1,102 @@
+import Notifier from '@core/utils/Notifier'
+
+import WorkshopClient from '@/api/workshop/workshop.client'
+import type { IUsePartnerStore } from '@/views/apps/workshop/externalservices/usePartner'
+import { usePartnerStore } from '@/views/apps/workshop/externalservices/usePartner'
+import type { IQueryPartner, PartnerType } from '@/views/apps/workshop/externalservices/types'
+
+export default class WorkshopService {
+  public readonly client: WorkshopClient
+  public readonly notifier: Notifier
+  public readonly store: IUsePartnerStore
+
+  constructor() {
+    this.client = new WorkshopClient()
+    this.notifier = new Notifier()
+    this.store = usePartnerStore()
+  }
+
+  async findAllPartner(query: IQueryPartner) {
+    this.store.loadingPartners = true
+    try {
+      const res = await this.client.findAll(query)
+
+      this.store.partners = res.data.data
+
+      this.notifier.success('Parceiros carregados com sucesso!')
+    }
+    catch (err: any) {
+      throw this.notifier.error(err.message)
+    }
+    finally {
+      this.store.loadingPartners = false
+    }
+  }
+
+  async findOnePartner(id: string) {
+    this.store.loadingPartners = true
+    try {
+      const res = await this.client.findOne(id)
+
+      this.store.findOne = res
+
+    }
+    catch (err: any) {
+      throw this.notifier.error(err.message)
+    }
+    finally {
+      this.store.loadingPartners = false
+    }
+  }
+
+  async addPartner(partner: PartnerType) {
+    this.store.loadingPartners = true
+    try {
+      await this.client.create(partner)
+      this.store.partners.rows.push(partner)
+
+      this.notifier.success('Parceiro adicionado com sucesso!')
+    }
+    catch (err: any) {
+      throw this.notifier.error(err.message)
+    }
+    finally {
+      this.store.loadingPartners = false
+    }
+  }
+
+  async updatePartner(id: string, partner: PartnerType) {
+    this.store.loadingPartners = true
+    try {
+      await this.client.update(id, partner)
+
+      const index = this.store.partners.findIndex(p => p.id === partner.id)
+
+      this.store.partners[index] = partner
+
+      this.notifier.success('Parceiro atualizado com sucesso!')
+    }
+    catch (err: any) {
+      throw this.notifier.error(err.message)
+    }
+    finally {
+      this.store.loadingPartners = false
+    }
+  }
+
+  async deletePartner(id: string) {
+    this.store.loadingPartners = true
+    try {
+      await this.client.delete(id)
+      this.store.partners = this.store.partners.rows.filter(p => p.id !== id)
+
+      this.notifier.success('Parceiro deletado com sucesso!')
+    }
+    catch (err: any) {
+      throw this.notifier.error(err.message)
+    }
+    finally {
+      this.store.loadingPartners = false
+    }
+  }
+}
