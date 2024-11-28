028b22e896b2ee52e7e4ba1cd475979b813f073f
Emerson <emerson.matosokl10@gmail.com>
2024-11-27 22:59:11 -0300

    git commit -m "feat(navigation):  Adicionando menus laterais na barra de navegação.
    - Oficinas e Costureiras
    - List
    - Add

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -1,10 +1,12 @@
 import home from './home'
 import rolePermissions from './role&Permissions'
 import shippingLabels from './shippingLabels'
+import workshop from './workshop'
 import type { VerticalNavItems } from '@/@layouts/types'
 
 export default [
   ...home,
   ...rolePermissions,
   ...shippingLabels,
+  ...workshop,
 ] as VerticalNavItems
