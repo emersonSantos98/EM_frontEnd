f329e3e217e0efb8e8ad2078e0c7b7c00d902b2d
Emerson <emerson.matosokl10@gmail.com>
2024-12-08 19:34:14 -0300

    commit(frontend): Adicionado componente de Tabs Para Estoque e Movimentações

---
+++ src/pages/apps/workshop/product/stock/index.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import StockList from "@/views/apps/workshop/stock/stockList.vue";
+import InventoryMovement from "@/views/apps/workshop/stock/inventoryMovement.vue";
+const currentTab = ref('tab-1');
+</script>
+
+<template>
+  <VCard>
+    <!-- Tabs -->
+    <VTabs
+      v-model="currentTab"
+      grow
+      stacked
+    >
+      <VTab value="tab-1">
+        <VIcon
+          icon="tabler-packages"
+          class="mb-2"
+        />
+        <span>Estoque</span>
+      </VTab>
+
+      <VTab value="tab-2">
+        <VIcon
+          icon="tabler-package-export"
+          class="mb-2"
+        />
+        <span>Movimentações</span>
+      </VTab>
+    </VTabs>
+
+    <!-- Tab Content -->
+    <VCardText>
+      <VWindow v-model="currentTab">
+        <!-- Estoque Tab -->
+        <VWindowItem value="tab-1">
+          <StockList />
+        </VWindowItem>
+
+        <!-- Movimentações Tab -->
+        <VWindowItem value="tab-2">
+          <InventoryMovement />
+        </VWindowItem>
+      </VWindow>
+    </VCardText>
+  </VCard>
+</template>
