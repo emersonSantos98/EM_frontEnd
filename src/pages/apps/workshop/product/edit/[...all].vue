94738b51680be6a52db3b88bf7e6293d57e17c2f
Emerson <emerson.matosokl10@gmail.com>
2025-01-24 23:41:57 -0300

    feat(frontend): index edite produto

---
+++ src/pages/apps/workshop/product/edit/[...all].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import EditProductView from '@/views/apps/workshop/product/EditProductView.vue'
+</script>
+
+<template>
+  <div>
+    <!-- Formulário de edição de produto -->
+    <EditProductView
+      class="mt-4 mb-12"
+    />
+  </div>
+</template>
