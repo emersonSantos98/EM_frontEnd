256e4e85a55f5f184b44fac684c137fb5e487427
Emerson <emerson.matosokl10@gmail.com>
2025-02-16 22:45:11 -0300

    feat(frontend): Add dashboard view with key metrics and product movement chart

---
+++ src/views/dashboard/components/LineChart.vue
@@ -0,0 +1,35 @@
+<template>
+  <canvas ref="chartRef"></canvas>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Chart from 'chart.js/auto'
+
+const chartRef = ref(null)
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
+
+onMounted(() => {
+  new Chart(chartRef.value, {
+    type: 'line',
+    data: {
+      labels: props.data.labels,
+      datasets: [{
+        label: 'Vendas Mensais',
+        data: props.data.data,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  })
+})
+</script>
